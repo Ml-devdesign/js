593dd8cbb08a7e9d241a0dfebd57d60ccd4c92f5
Leila Mahammed <devmldesign@gmail.com>
2024-03-17 20:56:14 +0100

    ok

--- Boucle/tableau/exo.js
+++ Boucle/tableau/exo.js
@@ -1,20 +1,50 @@
 // Exercice 1:
 // Créez un tableau vide appelé months. Ajoutez les douze mois de l'année en utilisant la méthode push.
+var months = [];
+months.push()
 
 // Exercice 2:
 // Écrivez une fonction qui prend un tableau de nombres et renvoie la somme de tous les éléments.
+var tableau = [5,6,5,8,,54,54,54,54,54,54,54,54,54];
+function somme(tableau) {
+    var somme = 0;
+    for (var i = 0; i < tableaau.length; i++) {
+        somme += tableaau[i];
+    }
+    return somme;
+}
 
 // Exercice 3:
 // Créez un tableau d'entiers. Doublez la valeur de chaque élément en utilisant la méthode map.
+tableau= tableau.map(n =>  n*2);
+console.log(tableau);
 
 // Exercice 4:
 // Écrivez une fonction qui prend un tableau de noms et renvoie une nouvelle liste de noms qui commencent par la lettre "A".
+tableaux = ['alice', 'charles', 'roger','anna'];
+
 
 // Exercice 5:
 // Créez deux tableaux, firstArray et secondArray, puis fusionnez-les en un seul tableau appelé mergedArray.
+let firstArray =[1,2,3,4,5,6,7,8,9,10]
+let secondArray =[1,2,3,4,5,6,7,8,9,10]
+let merged= firstArray.concat(secondArray)
+// merged.append(firstArray, secondArray)
+
+
 
 // Exercice 6:
 // Écrivez une fonction qui prend un tableau d'entiers et renvoie un nouveau tableau sans nombres pairs.
+function pairs(tableau){
+    var newArr = [];
+    for (var i = 0; i < tableau.length; i++) {
+        if (tableau[i] % 2 == 0) {
+            newArr.push(tableau[i]);
+        }
+    }
+    return newArr;
+}
+
 
 // Exercice 7:
 // Créez un tableau d'objets représentant des livres (avec des propriétés comme title, author, year). Filtrer le tableau pour obtenir une liste des livres publiés après 2000.
